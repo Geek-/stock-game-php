87ba2d2a9253552c51cf61146a3abb0a4bb53f88
Sam Brown <sam@datacurse.org>
2012-07-25 19:50:03 +0100

    stuff

--- includes/constants.php
+++ includes/constants.php
@@ -14,13 +14,13 @@
     define("DB_NAME", "jharvard_pset7");
 
     // your database's password
-    define("DB_PASSWORD", "crimson");
+    define("DB_PASSWORD", "Argentevenin2");
 
     // your database's server
     define("DB_SERVER", "localhost");
 
     // your database's username
-    define("DB_USERNAME", "jharvard");
+    define("DB_USERNAME", "root");
 
     // URL for Yahoo Finance
     define("YAHOO", "http://download.finance.yahoo.com/d/quotes.csv?f=snl1d1t1c1ohg&s=");
--- login.php
+++ login.php
@@ -2,7 +2,33 @@
 
     // require common code
     require_once("includes/common.php"); 
-
+    if (isset($_POST["action"])){
+      if (empty($_POST["username"]) || empty($_POST["password"]))
+        $error = true;
+      }
+      else{
+        $username = mysql_real_escape_string($_POST["username"]);
+        // prepare SQL
+        $sql = "SELECT * FROM users WHERE username='$username'";
+        // execute query
+        $result = mysql_query($sql);
+        // if we found user, check password
+        if (mysql_num_rows($result) == 1){
+          // grab row
+          $row = mysql_fetch_array($result);
+          // compare hash of user's input against hash that's in database
+          if (crypt($_POST["password"]) == $row["hash"]){
+            // remember that user's now logged in by caching user's ID in session
+            $_SESSION["id"] = $row["id"];
+            // redirect to portfolio
+            redirect("index.php");
+          }
+          else{
+            $error2=true;
+          }
+        }
+      }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +47,13 @@
       <a href="index.php"><img alt="C$50 Finance" height="110" src="images/logo.png" width="544"></a>
     </div>
     <div id="middle">
-      <form action="login2.php" method="post">
+      <? if($error): ?>
+        <div style="color:red;">One or more fields is empty!</div>
+      <? endif ?>
+      <? if($error2): ?>
+        <div style="color:red;">Incorrect password!</div>
+      <? endif ?>
+      <form action="login.php" method="post">
         <table>
           <tr>
             <td>Username:</td>
