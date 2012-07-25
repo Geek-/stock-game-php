eff61efe01e071f6eb04618e6e6f8863d61e4b35
Sam Brown <sam@datacurse.org>
2012-07-25 20:23:00 +0100

    it lives, badly but it lives...

--- includes/constants.php
+++ includes/constants.php
@@ -10,17 +10,17 @@
      **********************************************************************/
 
 
-    // your database's name (i.e., username_pset7)
-    define("DB_NAME", "jharvard_pset7");
+    // your database's name (i.e., username)
+    define("DB_NAME", "");
 
     // your database's password
-    define("DB_PASSWORD", "Argentevenin2");
+    define("DB_PASSWORD", "");
 
     // your database's server
     define("DB_SERVER", "localhost");
 
     // your database's username
-    define("DB_USERNAME", "root");
+    define("DB_USERNAME", "");
 
     // URL for Yahoo Finance
     define("YAHOO", "http://download.finance.yahoo.com/d/quotes.csv?f=snl1d1t1c1ohg&s=");
